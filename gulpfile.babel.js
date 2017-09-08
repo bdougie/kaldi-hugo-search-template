6a5230f9b81e1f876bc41973678b0691f61ab8d1
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2017-09-07 21:15:54 -0700

    working search overlay

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,7 @@ import gutil from "gulp-util";
 import postcss from "gulp-postcss";
 import cssImport from "postcss-import";
 import cssnext from "postcss-cssnext";
+import nestedcss from "postcss-nested";
 import BrowserSync from "browser-sync";
 import webpack from "webpack";
 import webpackConfig from "./webpack.conf";
@@ -69,6 +70,7 @@ gulp.task("css", () => (
   gulp.src("./src/css/*.css")
     .pipe(postcss([
       cssImport({from: "./src/css/main.css"}),
+      nestedcss(),
       cssnext(),
       cssnano(),
 
@@ -139,6 +141,7 @@ gulp.task("index-site", (cb) => {
         isRestricted = false,
         blacklist    = [
           "/page/",
+          "/tags/",
           "/tags/",
           "/pages/index.html",
           "/thanks",
--- site/static/js/liveSearch.js
+++ site/static/js/liveSearch.js
@@ -1,39 +1,185 @@
-// These keys are public and not sensitive. The first is the APP ID
-// The second is the Search Only Key.
-// const instantsearch = require("instantsearch.js");
+const algolia = algoliasearch("3TZ23PBKWA", "066dd636f08bd2fb78fcb120b38f7ddd");
+const algoliaIndex = algolia.initIndex("kaldi");
 
-var search = instantsearch({
-  appId: "3TZ23PBKWA",
-  apiKey: "066dd636f08bd2fb78fcb120b38f7ddd",
-  indexName: "kaldi",
-  urlSync: {}
+var $results
+var downcount = 0;
+var selectedSearchLink = '#';
+var url_string = window.location.href;
+var urlParam = url_string.indexOf("?search");
+var search_url = new URL(url_string);
+var url_query = search_url.searchParams.get("search");
+
+// show search overlay on load if search params in URL
+if (urlParam >= 0) {
+  $('#search').val(url_query).focus();
+  $('.search-icon').addClass('active');
+  $('#search-overlay').addClass('active');
+}
+
+
+// adds listener on the input field
+function initUI() {
+
+  $results = $("#results");
+
+  var ctrlDown = false,
+  ctrlKey = 17,
+  cmdKey = 91,
+  vKey = 86,
+  cKey = 67,
+  escKey = 27,
+  downArrow = 40,
+  upArrow = 38,
+  enterKey = 13;
+
+  $(document).keydown(function(e) {
+    if (e.keyCode == ctrlKey || e.keyCode == cmdKey) ctrlDown = true;
+  }).keyup(function(e) {
+    if (e.keyCode == ctrlKey || e.keyCode == cmdKey) ctrlDown = false;
+
+    if ($('.page input:focus, .footer input:focus, .page textarea:focus, .footer textarea:focus, .page select:focus, .footer select:focus').length === 0) {
+      $results.empty();
+
+      var downSelected    = false;
+      var clearSelected   = false;
+
+      if ($('#search-overlay').hasClass('active')) {
+        if (e.keyCode === escKey) {        // esc key clicked
+          $('#search-overlay').removeClass('active');
+        }
+        if (e.keyCode === downArrow) { // down arrow clicked
+          downSelected = true;
+        }
+        if (e.keyCode !== downArrow && e.keyCode !== upArrow) {
+          clearSelected = true;
+        }
+        if (e.keyCode === enterKey && $('#results li.selected')) {
+          window.location.href = selectedSearchLink;
+        }
+      }
+
+      var query = $('#search').val();
+
+      if (query.length > 0) {
+        $('#clear-search').addClass('active');
+      } else {
+        $('#clear-search').removeClass('active');
+      }
+
+      if (query.length < 1) {
+        return;
+      }
+
+      search(query, downSelected, clearSelected);
+    }
+  });
+}
+
+function search(query, downSelected, clearSelected) {
+  return algoliaIndex.search(query, function(err, content) {
+    renderResults(content.hits, query, downSelected, clearSelected);
+  });
+}
+
+function renderResults(results, query, downSelected, clearSelected) {
+  if (results && !results.length) {
+    $results.append("<p class='no-results'>Unfortunately, we can't find '" + query + "' - perhaps <a class='text-link' target='_blank' href='https://google.com'>The Internet</a> has what you're looking for?</p>");
+    return;
+  }
+
+  var pages = [],
+      blog = [];
+
+  // sort results into 3 categories
+  var sectionsInPages = ["", "products", "values", "contact"];
+  var sectionsInArticles = ["post"];
+
+  if (results !== undefined) {
+    results.forEach(function(result) {
+      // if (result.section === ['', 'press', 'pricing', 'support', 'tags'] || result.section === (result.title).toLowerCase() || result.title === 'Site of the Week') {
+      if (sectionsInPages.indexOf(result.section) > -1 || result.section === (result.title).toLowerCase()) {
+        pages.push(result);
+      } else if (sectionsInArticles.indexOf(result.section) > -1) {
+        blog.push(result);
+      }
+    });
+
+    if (pages.length) {
+      $results.append("<ul class='pages-results search-column'><li class='section-label'>Pages</li></ul>");
+    }
+    pages.slice(0, 7).forEach(function(result) {
+      var $result = $("<li>");
+      $result.append($("<a>", {
+        href: result.href,
+        text: decodeHtml(result.title)
+      }));
+      $('.pages-results').append($result);
+    });
+
+    if (blog.length) {
+      $results.append("<ul class='blog-results search-column'><li class='section-label'>Articles</li></ul>");
+    }
+    blog.slice(0, 7).forEach(function(result) {
+      var $result = $("<li>");
+      $result.append($("<a>", {
+        href: result.href,
+        text: decodeHtml(result.title)
+      }));
+      $('.blog-results').append($result);
+    });
+
+    if (clearSelected) {
+      downcount = 0;
+    }
+
+    if (downSelected) {
+
+      if (downcount > 0 ) {
+        $('#results li:not(.section-label)').eq(downcount).addClass('selected');
+      } else {
+        $('#results li:not(.section-label)').first().addClass('selected');
+      }
+      selectedSearchLink = $('#results li:not(.section-label).selected a').attr('href');
+
+      if ($('#results li:not(.section-label)').last().hasClass('selected') || downcount < 0) {
+        downcount = 0;
+      } else {
+        downcount++;
+      }
+    }
+
+    function decodeHtml(html) {
+      var txt = document.createElement("textarea");
+      txt.innerHTML = html;
+      return txt.value;
+    }
+  }
+}
+
+$(document).ready(function() {
+  initUI();
 });
 
-// initialize RefinementList
-search.addWidget(
-  instantsearch.widgets.refinementList({
-    container: "#refinement-list",
-    attributeName: "category"
-  })
-);
+$('#clear-search').click(function() {
+  $('#search').val('').focus();
+  $(this).removeClass('active');
+  $('.search-column').remove();
+});
 
-// initialize SearchBox
-search.addWidget(
-  instantsearch.widgets.searchBox({
-    container: "#search-box",
-    placeholder: "Search the site"
-  })
-);
+$('.search-button').click(function() {
+  $('.search-icon').toggleClass('active');
+  $('#search-overlay').toggleClass('active');
 
-// initialize hits widget
-search.addWidget(
-  instantsearch.widgets.hits({
-    container: "#hits",
-    templates: {
-      empty: 'No results',
-      item: "<a href={{href}}><em>{{objectID}}</em> {{{_highlightResult.content.value}}}</a>"
+  if ($('#search-overlay').hasClass('active')) {
+    window.scrollTo(0, 0);
+    $('#search').focus();
+  }
+
+  if ($('.landing.page').length) {
+    if ($('#search-overlay').hasClass('active')) {
+      $('#header').removeClass('centered');
+    } else {
+      $('#header').addClass('centered');
     }
-  })
-);
-
-search.start();
+  }
+});
